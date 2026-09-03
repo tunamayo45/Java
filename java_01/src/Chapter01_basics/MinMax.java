c6a83caaba787378531ad2099b6c56baf9617a0c
tunamayo <tunamayo45.4@gmail.com>
2026-09-03 19:49:51 +0900

    변수의 min,max를 알 수 있다.

---
+++ java_01/src/Chapter01_basics/MinMax.java
@@ -0,0 +1,32 @@
+package Chapter01_basics;
+
+public class MinMax {
+
+	public static void main(String[] args) {
+		
+//		최대값, 최소값
+		
+		System.out.println("byte :"+Byte.MIN_VALUE+" - "+Byte.MAX_VALUE);
+		
+		System.out.println("short :"+Short.MIN_VALUE+" - "+Short.MAX_VALUE);
+		
+		System.out.println("int :"+Integer.MIN_VALUE+" - "+Integer.MAX_VALUE);
+		
+		System.out.println("Long :"+Long.MIN_VALUE+" - "+Long.MAX_VALUE);
+		
+		System.out.println("float :"+Float.MIN_VALUE+" - "+Float.MAX_VALUE);
+		
+		System.out.println("double :"+Double.MIN_VALUE+" - "+Double.MAX_VALUE);
+		
+		/*
+		 	byte :-128 - 127
+			short :-32768 - 32767
+			int :-2147483648 - 2147483647
+			Long :-9223372036854775808 - 9223372036854775807
+			float :1.4E-45 - 3.4028235E38
+			double :4.9E-324 - 1.7976931348623157E308
+		*/
+
+	}
+
+}
