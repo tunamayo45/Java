7bccb7d4dc0f909f7fc3c608e1e6fda77c8d5a0b
tunamayo <tunamayo45.4@gmail.com>
2026-09-04 23:49:22 +0900

    casting 연산식

--- java_01/src/Chapter01_basics/casting.java
+++ java_01/src/Chapter01_basics/casting.java
@@ -39,6 +39,46 @@ public class casting {
 		flValue = (float)doValue;
 		System.out.println(flValue);
 		
+		/* 연산 자동 변환
+		 * 		정수 타입 변수가 산술에서 피연산자로 사용되는 경우
+		 * 		byte,char,short 타입 변수는 int 으로 promotion
+		 * 
+		 * 		double 도 같은 연산 과저을 거침
+		*/
+		
+		inValue = chValue + 10;
+		System.out.println(inValue);
+		
+		inValue = chValue + chValue;
+		System.out.println(inValue);
+		
+		
+		doValue = inValue + doValue;
+		System.out.println(doValue);
+		
+		doValue = 1 / (double)2;			// 정수에서 실수 값을 구하고 싶다면 한쪽을 casting을 해 줘야 한다. 
+		System.out.println(doValue);
+		
+		doValue = 1 / 2;
+		System.out.println(doValue);
+		
+		
+		/*연산자 문자열 자동 변환
+		 * 		연산자는 피연산자가 모두 숫자 일 경우 덧셈 연산 수행
+		 * 		연산자중 문자열이 있으면 문자열로 변환 후 문자열을 결합니다.
+		 * 		+ 가 여러개 이면 순자적으로 덧셈을 하다 문자열을 만나는 순간 문자열로 변환이 된다.
+		*/
+		
+		 inValue = 1+2;
+		 System.out.println(inValue);
+		 
+		 String stValue= "1" + 1;
+		 System.out.println(stValue);
+		 
+		 stValue = 1 + 1 + "1";
+		 System.out.println(stValue);
+		
+		 
 	}
 
 }
