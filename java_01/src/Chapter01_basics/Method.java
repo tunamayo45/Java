c264d2819219b102b2189f20d1d903ec3607e00a
tunamayo <tunamayo45.4@gmail.com>
2026-09-04 23:45:41 +0900

    method의 기본 구성

---
+++ java_01/src/Chapter01_basics/Method.java
@@ -0,0 +1,36 @@
+package Chapter01_basics;
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+		/* 동작을 실행해 주는 주는 무언가 
+		 * 
+		 * [반환 타입] [메서드 이름] ([매개변수]){
+		 * 		[실행 문];
+		 * 		return [반환문];
+		 * }
+		 */
+		
+//		반환 타입, 매개변수
+		String 토스터기 (String 빵) {
+			return "구운 + 빵";
+		}
+
+//		반환 타입
+		String 번호표기계 () {
+			return 번호;
+		}
+		
+//		매개변수
+		Void 저금통 (int 동전) {
+			System.err.println(동정+"저금");
+		}
+		
+//		
+		Void 호출밸 () {
+			System.out.println("호출");
+		}
+	}
+
+}
