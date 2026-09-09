78b4c4d22ee41845bb15984f0a2878a33114a8e0
tunamayo <tunamayo45.4@gmail.com>
2026-09-09 13:16:22 +0900

    간단함 method 사용법

--- java_01/src/Chapter01_basics/Toaster.java
+++ java_01/src/Chapter01_basics/Toaster.java
@@ -7,12 +7,15 @@ public class Toaster {
 //	타입이 존재하지 않을 때 void(공허, 하무것도 없다.)
 	
 	public static void main(String[] args) {
-		String dish = toaster("빵"); //
+		// toaster 라는 method를 호출하는 동시에 매개변수에 값을 전달 한다.
+		// dish에 toaster 에 반환 값을 저장 한다
+		String dish = toaster("빵");
 		System.out.println(dish);
 	}
 	
-	static String toaster(String input) {
-		System.out.println(input + "구워진다.");
-		return "구워진 "+ input;
+//	↱꼭 써야 하지만 아직 모름			↱매개변수 타입, 이름
+	static String toaster(String input) { // toaster 을 부른 곳에서 매개변수 보내오면 이 곳에 저장됨
+		System.out.println(input + " 구워진다."); // 매변수를 가져와 사용
+		return "구워진 "+ input;// method 를 부른 곳 에 반환값 전달.
 	}
 }
