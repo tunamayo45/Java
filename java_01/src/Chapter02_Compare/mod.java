d6c08f2a3701de6daa771dd4f212ccfe3f57d6b5
tunamayo <tunamayo45.4@gmail.com>
2026-09-09 14:14:42 +0900

    %는 나머지 값

---
+++ java_01/src/Chapter02_Compare/mod.java
@@ -0,0 +1,15 @@
+package Chapter02_Compare;
+
+public class mod {
+
+	public static void main(String[] args) {
+		
+//		%, mod 연산, 나머지 연산
+//		두 개 항을  나눈 나머지 값을 구한다.
+//		%는 홀,짝 구분 또는 배수 찾기에 자주 사용된다.
+		
+		System.out.println(" 5 % 2 = " +5%2);
+
+	}
+
+}
